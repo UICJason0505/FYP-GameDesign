55fad385c7f985d765d604e3b4ad3bef206fbd37
Jason <ljq1344048113@gmail.com>
2025-06-11 15:00:23 +0800

    chess prefab

--- Assets/Scripts/Chess.cs
+++ Assets/Scripts/Chess.cs
@@ -2,8 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class Chess : MonoBehaviour
 {
+    public int number = 2;
     // Start is called before the first frame update
     void Start()
     {
