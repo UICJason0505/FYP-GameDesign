f1d73e9ac8eb2bf6b9b4b43136cd902e3bc79eed
YoTH <3261687387@qq.com>
2025-06-12 15:39:47 +0800

    预制件位置？

--- Assets/Scripts/UnitSpawner.cs
+++ Assets/Scripts/UnitSpawner.cs
@@ -2,12 +2,12 @@ using UnityEngine;
 
 public class UnitSpawner : MonoBehaviour
 {
-    public GameObject unitPrefab; // 拖入你的棋子 prefab
+    public GameObject unitPrefab;
     private HexTile pendingTile = null;
 
     void Update()
     {
-        // 左键点击检测
+        // 鼠标点击空格子
         if (Input.GetMouseButtonDown(0))
         {
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
@@ -19,23 +19,24 @@ public class UnitSpawner : MonoBehaviour
                 if (tile != null && !tile.IsOccupied)
                 {
                     pendingTile = tile;
-                    Debug.Log("点击格子：按 N 生成棋子");
+                    Debug.Log("点击了空格子，按 N 生成棋子");
                 }
                 else
                 {
-                    pendingTile = null; // 点击其他内容，取消提示
+                    pendingTile = null;
                 }
             }
         }
 
-        // 按 N 生成棋子
+        // 按 N 键生成棋子
         if (pendingTile != null && Input.GetKeyDown(KeyCode.N))
         {
-            Vector3 spawnPos = pendingTile.GetTopCenter();
+            Vector3 spawnPos = pendingTile.transform.position + Vector3.up * 0.5f;
+
             GameObject unit = Instantiate(unitPrefab, spawnPos, Quaternion.identity);
             pendingTile.IsOccupied = true;
 
-            Debug.Log("棋子已生成");
+            Debug.Log("棋子已生成于：" + spawnPos);
             pendingTile = null;
         }
     }
