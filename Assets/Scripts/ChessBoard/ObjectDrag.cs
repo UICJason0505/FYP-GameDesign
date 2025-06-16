831fbb94d878864e5febcbbf99d4613128cf3783
s230026146 <s230026146@mail.uic.edu.cn>
2025-06-16 12:04:30 +0800

    modifying by Tong

--- Assets/Scripts/ChessBoard/ObjectDrag.cs
+++ Assets/Scripts/ChessBoard/ObjectDrag.cs
@@ -4,15 +4,6 @@ using UnityEngine;
 
 public class ObjectDrag : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    private Vector3 mouseOffset;
 }
+
