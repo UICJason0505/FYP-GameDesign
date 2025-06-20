f34ba17b2552cff36b37478d3b03a89100ded7e7
YoTH <3261687387@qq.com>
2025-06-20 17:28:14 +0800

    开始场景

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -5,6 +5,19 @@ public class MainMenu : MonoBehaviour
 {
     public void StartGame()
     {
-        SceneManager.LoadScene("SampleScene");
+        SceneManager.LoadScene("Main");
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("QuitGame 被调用");
+
+#if UNITY_EDITOR
+        // 如果是在 Unity 编辑器中运行
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        // 如果是打包后的游戏
+        Application.Quit();
+#endif
     }
 }
