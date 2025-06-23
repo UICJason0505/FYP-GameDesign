bcea68b98de44b5a052057b93756d1d83a094da6
YoTH <3261687387@qq.com>
2025-06-23 21:44:32 +0800

    扣除待定

--- Assets/Scripts/Chess/King.cs
+++ Assets/Scripts/Chess/King.cs
@@ -23,6 +23,34 @@ public class King : MonoBehaviour
         {
             GridBuildingSystem.Instance.SpawnChess2();
         }
+        /*
+
+        var player = TurnManager.Instance.players[TurnManager.Instance.CurrentPlayerIndex];
+
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            if (player.HasEnoughActionPoints(2))
+            {
+                GridBuildingSystem.Instance.SpawnChess1();
+                player.UseActionPoint(2);
+            }
+            else
+            {
+                Debug.Log("行动点不足，不能召唤近战棋子");
+            }
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            if (player.HasEnoughActionPoints(2))
+            {
+                GridBuildingSystem.Instance.SpawnChess2();
+                player.UseActionPoint(2);
+            }
+            else
+            {
+                Debug.Log("行动点不足，不能召唤远程棋子");
+            }
+        }*/
     }
     void OnMouseDown()
     {
